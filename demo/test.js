2faeaa811f616442937295f8c9539d12a461a8ab
Jackson Tian <shyvo1987@gmail.com>
2014-01-20 14:33:20 +0800

    添加测试用例

--- demo/test.js
+++ demo/test.js
@@ -1,19 +1,44 @@
-var request = require('request');
 var lame = require('lame');
 var Speaker = require('speaker');
 
+var getUrlLib = function (url) {
+  if (url.indexOf('https') !== -1) {
+    return require('https');
+  }
+  return require('http');
+};
+
 var src = 'http://zhangmenshiting.baidu.com/data2/music/10470876/7343701219600128.mp3?xcode=351634fd3718fed5abb2f9389b9d4097b9319fcd4157c2b2';
 var src = 'http://zhangmenshiting.baidu.com/data2/music/33909933/31627254108000128.mp3?xcode=0bad1c5e5224e56ff31000774b753c9492822cd485d59822';
-var res = request.get(src);
-var decoder = new lame.Decoder();
-var speaker = new Speaker();
-res.pipe(decoder).pipe(speaker);
-res.on('end', function () {
-  console.log('res end', new Date());
+// var src = 'http://nodejs.org/api/http.html#http_http_get_options_callback';
+var urllib = getUrlLib(src);
+var request = urllib.get(src, function (res) {
+  if (res.statusCode !== 200) {
+    console.log(new Error('资源响应错误'));
+    return;
+  }
+  console.log(res.headers);
+  console.log("Got response: " + res.statusCode);
+  var decoder = new lame.Decoder();
+  var speaker = new Speaker();
+  res.pipe(decoder).pipe(speaker);
+  res.on('end', function () {
+    console.log('res end', new Date());
+  });
+  decoder.on('end', function () {
+    console.log('decoder end', new Date());
+  });
+  speaker.on('close', function () {
+    console.log('speaker close', new Date());
+  });
 });
-decoder.on('end', function () {
-  console.log('decoder end', new Date());
+
+request.on('error', function(e) {
+  console.log(e);
+  console.log("Got error: " + e.message);
 });
-speaker.on('close', function () {
-  console.log('speaker close', new Date());
-});
+
+// Content-Disposition:attachment; filename="ÎÒµÄ¸èÉùÀï.mp3"
+// Content-Length:3468756
+// Content-MD5:1453561f58b606eb30ba7e281b039d4e
+// Content-Type:audio/mpeg
---
+++ test/player.test.js
@@ -0,0 +1,208 @@
+var path = require('path');
+var os = require('os');
+var fs = require('fs');
+
+var should = require('should');
+var IPod = require('../');
+
+describe('new IPod', function () {
+  it('new IPod([], {})', function () {
+    var ipod = new IPod(['url'], {mode: 'random'});
+    ipod.list.should.be.eql(['url']);
+    ipod.mode.should.be.equal('random');
+  });
+
+  it('new IPod([])', function () {
+    var ipod = new IPod(['url']);
+    ipod.list.should.be.eql(['url']);
+    ipod.mode.should.be.equal('loop');
+  });
+
+  it('new IPod()', function () {
+    var ipod = new IPod();
+    ipod.list.should.be.eql([]);
+    ipod.mode.should.be.equal('loop');
+  });
+
+  it('new IPod(song)', function () {
+    var ipod = new IPod('url');
+    ipod.list.should.be.eql(['url']);
+    ipod.mode.should.be.equal('loop');
+  });
+
+  it('new IPod({})', function () {
+    var ipod = new IPod({mode: 'random'});
+    ipod.list.should.be.eql([]);
+    ipod.mode.should.be.equal('random');
+  });
+});
+
+describe('download', function () {
+  var filepath = path.join(os.tmpdir(), 'cached_file');
+
+  beforeEach(function (done) {
+    fs.unlink(filepath, function () {
+      done();
+    });
+  });
+  var ipod = new IPod();
+  it('download error url', function (done) {
+    ipod.download('http://some.url/filename.mp3', filepath, function (err) {
+      should.exist(err);
+      err.should.have.property('message', 'getaddrinfo ENOTFOUND');
+      done();
+    });
+  });
+
+  it('download 404 url', function (done) {
+    var url = 'http://zhangmenshiting.baidu.com/data2/music/33909933/31627254108000128.mp31?xcode=0bad1c5e5224e56ff31000774b753c9492822cd485d59822';
+    ipod.download(url, filepath, function (err) {
+      should.exist(err);
+      err.should.have.property('message', 'resource invalid');
+      done();
+    });
+  });
+
+  it('download html url', function (done) {
+    var url = 'http://www.baidu.com/';
+    ipod.download(url, filepath, function (err) {
+      should.exist(err);
+      err.should.have.property('message', 'resource type is unsupported');
+      done();
+    });
+  });
+
+  it('download ok url', function (done) {
+    var url = 'http://zhangmenshiting.baidu.com/data2/music/33909933/31627254108000128.mp3?xcode=0bad1c5e5224e56ff31000774b753c9492822cd485d59822';
+    ipod.download(url, filepath, function (err, readable) {
+      should.not.exist(err);
+      readable.should.have.property('readable', true);
+      done();
+    });
+  });
+});
+
+describe('nextSong', function () {
+  var list = ['1', '2', '3', '4', '5'];
+  
+  it('loop mode', function () {
+    var ipod = new IPod(list, {mode: 'loop'});
+    ipod.nextSong();
+    ipod.currentIndex.should.be.equal(0);
+    ipod.nextSong();
+    ipod.currentIndex.should.be.equal(1);
+    ipod.nextSong();
+    ipod.currentIndex.should.be.equal(2);
+    ipod.nextSong();
+    ipod.currentIndex.should.be.equal(3);
+    ipod.nextSong();
+    ipod.currentIndex.should.be.equal(4);
+    ipod.nextSong();
+    ipod.currentIndex.should.be.equal(0);
+  });
+
+  it('random mode', function () {
+    var ipod = new IPod(list, {mode: 'random'});
+    var order = [];
+    ipod.nextSong();
+    order.push(ipod.currentIndex);
+    ipod.nextSong();
+    order.push(ipod.currentIndex);
+    ipod.nextSong();
+    order.push(ipod.currentIndex);
+    // 这样的概率应该很小才是
+    order.should.not.eql([0, 1, 2]);
+  });
+
+  it('order mode', function () {
+    var ipod = new IPod(list, {mode: 'order'});
+    var order = [];
+    ipod.nextSong();
+    order.push(ipod.currentIndex);
+    ipod.nextSong();
+    order.push(ipod.currentIndex);
+    ipod.nextSong();
+    order.push(ipod.currentIndex);
+    ipod.nextSong();
+    order.push(ipod.currentIndex);
+    ipod.nextSong();
+    order.push(ipod.currentIndex);
+    ipod.nextSong();
+    order.push(ipod.currentIndex);
+    // 这样的概率应该很小才是
+    order.should.be.eql([0, 1, 2, 3, 4, 5]);
+  });
+});
+
+describe('add', function () {
+  var list = ['1', '2'];
+
+  it('should ok', function () {
+    var ipod = new IPod(list);
+    ipod.add('hehe');
+    ipod.list.should.be.eql(['1', '2', 'hehe']);
+  });
+});
+
+describe('addFolder', function () {
+  it('should ok', function (done) {
+    var ipod = new IPod();
+    ipod.addFolder(path.join(__dirname, '../demo'), function (err) {
+      should.not.exist(err);
+      ipod.list.should.have.length(2);
+      done();
+    });
+  });
+
+  it('should not ok', function (done) {
+    var ipod = new IPod();
+    ipod.addFolder(path.join(__dirname, '../inexist'), function (err) {
+      should.exist(err);
+      err.should.have.property('code', 'ENOENT');
+      done();
+    });
+  });
+});
+
+describe('read', function () {
+  it('should be ok', function (done) {
+    var ipod = new IPod();
+    ipod.read(path.join(__dirname, '../demo/demo.mp3'), function (err, readable) {
+      should.not.exist(err);
+      readable.should.have.property('readable', true);
+      done();
+    });
+  });
+
+  describe('read from net', function () {
+    before(function (done) {
+      fs.unlink(path.join(os.tmpdir(), '31627254108000128.mp3'), function () {
+        done();
+      });
+    });
+
+    it('should be ok with net', function (done) {
+      var ipod = new IPod(  {
+        downloads: os.tmpdir()
+      });
+      var url = 'http://zhangmenshiting.baidu.com/data2/music/33909933/31627254108000128.mp3?xcode=0bad1c5e5224e56ff31000774b753c9492822cd485d59822';
+      ipod.read(url, function (err, readable) {
+        should.not.exist(err);
+        readable.should.have.property('readable', true);
+        done();
+      });
+    });
+
+    it('should be ok with cache', function (done) {
+      var ipod = new IPod(  {
+        downloads: path.join(__dirname, '../demo')
+      });
+      var url = 'http://some.url/demo.mp3';
+      ipod.read(url, function (err, readable) {
+        should.not.exist(err);
+        readable.should.have.property('readable', true);
+        done();
+      });
+    });
+  });
+});
